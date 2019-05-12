f1944bf4f9502e2e17de88e5dc3d5da95eabc1fb
Merey <merey.90@gmail.com>
2019-05-12 23:56:42 +0200

    minor fixes to product controller

--- src/modules/product/controllers/index.ts
+++ src/modules/product/controllers/index.ts
@@ -1,7 +1,8 @@
-import { readFromFile } from '../util/readFromFile';
+import { Response, NextFunction, Request } from 'express';
+
 import { getMappedData } from '../util/getMappedData';
 
-export const getProducts = async (req, res, next) => {
+export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
   const products = await getMappedData();
   res.send(products);
 };
